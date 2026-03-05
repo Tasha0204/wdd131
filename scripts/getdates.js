1663f847031ea21ca4475ab1a738049468235884
Tasha-Vidal <tvidalbravo@byupathway.edu>
2026-03-05 11:13:05 -0500

    initial commit

---
+++ scripts/getdates.js
@@ -0,0 +1,7 @@
+
+let yr = new Date().getFullYear();
+let lastModified = document.lastModified;
+let author = "Tasha Vidal";
+let place = "Lima, Perú";
+document.getElementById("currentyear").textContent = yr;
+document.getElementById("lastModified").textContent = `Last Modification: ${lastModified}`;
