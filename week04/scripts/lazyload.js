bb633eec6de3de49ec75864f3a0d0074db7374db
Tasha-Vidal <tvidalbravo@byupathway.edu>
2026-03-26 09:32:17 -0500

    commit

---
+++ week04/scripts/lazyload.js
@@ -0,0 +1,4 @@
+document.getElementById('lastModified').textContent = document.lastModified;
+
+document.getElementById('welcomeMessage').textContent = "Welcome to the " + courseName + " " + stuFirstName + "!";
+
